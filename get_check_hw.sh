f49666c6d21a081fcf37f1530a8fd441e8d86ad4
Alex Zaidelson <git@zaidelson.com>
2025-09-18 12:56:08 +0300

    Update get_check_hw.sh

--- get_check_hw.sh
+++ get_check_hw.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 wget -q https://raw.githubusercontent.com/scrtlabs/test1/master/check_hw_1.22_devnet.gz
-gunzip check_hw_1.22_devnet.gz
+tar -xzvf check_hw_1.22_devnet.gz
 chmod +x check-hw/check-hw
 ./check-hw/check-hw
