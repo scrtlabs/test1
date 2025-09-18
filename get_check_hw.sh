74beb16eb180153b1338482fcf293128cb408215
valdok <vgelfer@gmail.com>
2025-09-18 16:04:54 +0300

    Delete get_check_hw.sh

--- get_check_hw.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-wget -q https://raw.githubusercontent.com/scrtlabs/test1/master/check_hw_1.22_devnet.gz
-tar -xzvf check_hw_1.22_devnet.gz
-chmod +x check-hw/check-hw
-./check-hw/check-hw
